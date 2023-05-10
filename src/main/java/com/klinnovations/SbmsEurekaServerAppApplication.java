1f0757cb9ca82ffd5763bc7d52b0fb845ba9cfba
Gaini Laxman <gainilaxman12@gmail.com>
2023-05-10 19:56:48 +0530

    Eureka Server Created

---
+++ src/main/java/com/klinnovations/SbmsEurekaServerAppApplication.java
@@ -0,0 +1,15 @@
+package com.klinnovations;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class SbmsEurekaServerAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbmsEurekaServerAppApplication.class, args);
+	}
+
+}
